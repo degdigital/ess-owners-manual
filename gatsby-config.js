f1411f6f1d574b341813c73de2fdb0e470f4aa59
Aaron Ladage <aladage@degdigital.com>
2020-08-06 11:27:25 -0500

    removed gatsby starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ESS Owner's Manual`,
+    description: `ESS Owner's Manual`,
+    author: `@aladage`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,8 +10,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -24,18 +24,18 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `lbmfhl46kyiv`,
-        accessToken: `cYC53WBUJ1sG7PEMOaRB1yMV2FcFcVTsEE7EKUrg1vc`,
-      },
-    },
+        accessToken: `cYC53WBUJ1sG7PEMOaRB1yMV2FcFcVTsEE7EKUrg1vc`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
